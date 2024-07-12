ea90978aab8fd81e5676a5069a71c83d7187b016
zeeshanbiit <zeeshanbiit786@gmail.com>
2024-07-13 04:15:52 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Muhammad Zeshan on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
