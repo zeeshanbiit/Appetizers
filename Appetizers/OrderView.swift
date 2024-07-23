668d288b09853bc2c58b73a175c54d9c75b17648
zeeshanbiit <zeeshanbiit786@gmail.com>
2024-07-24 03:47:32 +0500

    Additing Full ScreenCover Functionality SwiftUI

--- Appetizers/OrderView.swift
+++ Appetizers/OrderView.swift
@@ -8,10 +8,19 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var isPresented = false
     var body: some View {
         
         NavigationStack{
             Text("Order View")
+            Button{
+                isPresented = true
+            }label: {
+                Text("Order Detail")
+            }.fullScreenCover(isPresented: $isPresented, content: {
+                AccountView()
+            })
                 .navigationTitle("Order View")
         }
        
