33ade3e223e65ffce7830d5197f61848bd07f4b2
zeeshanbiit <zeeshanbiit786@gmail.com>
2024-07-25 03:13:08 +0500

    Started With MVVM Pattern in SwiftUI

---
+++ Appetizers/AppetizerModel.swift
@@ -0,0 +1,14 @@
+//
+//  AppetizerModel.swift
+//  Appetizers
+//
+//  Created by Muhammad Zeshan on 25/07/2024.
+//
+
+import Foundation
+
+struct AppetizerModel : Codable{
+    
+    var Id = UUID()
+    
+}
---
+++ Appetizers/AppetizerViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AppetizerViewModel.swift
+//  Appetizers
+//
+//  Created by Muhammad Zeshan on 25/07/2024.
+//
+
+import Foundation
+
+class AppetizerViewModel : ObservableObject{
+    
+    @Published var isPresented = false
+    
+}
